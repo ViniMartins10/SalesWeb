1e109cc8e3afbf0c46a11b33f4b72b0725562efe
Vinicius Martins <viniciusmvg@hotmail.com>
2026-07-30 18:28:56 -0300

    Asynchronous operations using Tasks (async, await)

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -25,8 +25,8 @@ namespace SalesWebMVC.Models
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
 
-        public int DepartmentId { get; set; }
-        public Department Department{ get; set; }
+        public int ?DepartmentId { get; set; }
+        public Department ?Department{ get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
